fa119d7336511fbf4e5b42802d21a78850b6e096
hithenull <3041315798@qq.com>
2019-02-26 10:06:03 +0800

    aa

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,8 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		System.out.println("11111");
-		System.out.println("11111");
 		
 		System.out.println("99999");
 	}
